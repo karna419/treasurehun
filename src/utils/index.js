5086be95bc057b7c99c68c1141c2844e51c28bac
karunakaran7415@gmail.com <Karna@419>
2020-08-16 11:59:55 +0530

    initial

---
+++ src/utils/index.js
@@ -0,0 +1,45 @@
+import * as icons from 'assets/icons';
+
+export const userDetails = [
+  {
+    title: 'user one',
+    subTitle: 'Details and description',
+    location: {
+      lat: 37.4218893,
+      lng: 122.101602,
+    },
+  },
+  {
+    title: 'user Two',
+    subTitle: 'Details and description',
+    location: {
+      lat: 37.42101134,
+      lng: 122.93626,
+    },
+  },
+  {
+    title: 'user Three',
+    subTitle: 'Details and description',
+    location: {
+      lat: 37.4210003,
+      lng: 122.101602,
+    },
+  },
+  {
+    title: 'user Four',
+    subTitle: 'Details and description',
+    location: {
+      lat: 37.1118893,
+      lng: 122.101602,
+    },
+  },
+];
+
+export const claimDetails = [
+  {numberImage: icons.messageOne, title: ' treasure'},
+  {numberImage: icons.messageTwo, title: 'o find.'},
+  {numberImage: icons.messageThree, title: 'Tthe black circle in the map'},
+  {numberImage: icons.messageFour, title: 'Scan the treasure'},
+  {numberImage: icons.messageFive, title: 'chances)'},
+  {numberImage: icons.messageSix, title: 'You will r Payrking days'},
+];
